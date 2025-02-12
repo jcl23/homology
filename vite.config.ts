6660ee4fec4bcc4bd63ebaa1598f27b52767e1bb
jcl23 <justinclee.email@gmail.com>
2025-02-11 22:01:09 -0800

    Github and vite fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'src/index.html'),  // Ensure this points to your index.html
+    },
+  },
+  base: '/homology/'
 })
